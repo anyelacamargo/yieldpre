cb159e56d7080540b2083ceebb0ee78a19af6383
acamargo <acamargo@rocketmail.com>
2016-12-20 14:34:54 +0000

    More code

--- yieldAnalytics.R
+++ yieldAnalytics.R
@@ -15,12 +15,29 @@ rhsfit <- function(varray, mydata) {
   sum((mydata$y - (varray[1] + (varray[2] * mydata$x) + (varray[3] *(exp(-varray[4]*mydata$x)))))^2)
 }
 
-fstress = function ()
+# Effect of Soil wate
+# Richter et al. Agricultural and forest, 109 (2001)
+soilWaterStress = function ()
 {
-  
-  fs = 2 
+  Oth = 0.02; #(Sinclair (1986))
+  fdt = 6:12; #(Richter)
+  Orel = DAW / AWC;
+  fstress = (2 / (1 + exp(-fdt * (Orel - Oth)))) - 1;
+  return(fstress);
   
 }
+
+# Quantitative effect of fstress on y (f on A. Qi)
+effectofSWSfitYield = function(fstress)
+{
+  p = list()
+  v0 = 0.005866;
+  umin0 = -0.000169;
+  p$umin = umin0*(2 - fstress);
+  p$v = v0*(1 + 0.1*(1 - fstress));
+  return(p);
+}
+  
 
 apparentGrowthRate = function()
 {
@@ -61,7 +78,7 @@ extractParam = function(model)
   p = list();
   param = data.frame(summary(model)$parameters);
   p$umin = param['B.B',1];
-  p$tmax = (1/param['r.r',1]) * log((param['r.r',1] * param['C.C',1]) / param['B.B',1]);
+  p$tmax = (1/param['v.v',1]) * log((param['v.v',1] * param['C.C',1]) / param['B.B',1]);
   return(p);
 }
 
@@ -70,12 +87,12 @@ extractParam = function(model)
 # Foliage Cover, Model I
 # ydat = yield
 # tdat = accumulated temperature
-# Annals of Botany 79, 1997. Wekker and Jaggard 
+# Wekker and Jaggard. Annals of Botany 79 (1997) 
 fitYield = function(ydat, tdat)
 {
   wdata = data.frame(y=ydat, x=tdat);
-  start1 = c(A = 1, B = 1, C = 0, r=0);
-  eunsc = ydat ~ A + B*tdat + C*(exp(-r*tdat));
+  start1 = c(A = 1, B = 1, C = 0, v=0);
+  eunsc = ydat ~ A + B*tdat + C*(exp(-v*tdat));
   optx = optima(start1, wdata, rhsfit);
   o = nlsLM(eunsc, 
             start=list(A = start1[1], B = start1[2], C = start1[3], r = start1[4]), trace = TRUE);
