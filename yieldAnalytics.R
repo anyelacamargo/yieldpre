61906dac85345a2aab1dc411dbefe24bbd88dddd
anyelacamargo <anyelavcamargo@gmail.com>
2017-02-03 17:00:08 +0000

    Added parameters and more code

--- yieldAnalytics.R
+++ yieldAnalytics.R
@@ -1,23 +1,27 @@
 library('minpack.lm');
 library("optimx")
-library(ggplot2);
+library('ggplot2');
 
 
 optima = function(start1, w, fc)
 {
   ## Optim
   w.optx <- optimx(par=start1, fn=fc, mydata=w, 
-                   control=list(all.methods=TRUE, save.failures=TRUE, maxit=100))
+                   control=list(all.methods=TRUE, save.failures=TRUE, maxit=1000))
   return(w.optx)
 }
 
-rhsfit <- function(varray, mydata) {
-  sum((mydata$y - (varray[1] + (varray[2] * mydata$x) + (varray[3] *(exp(-varray[4]*mydata$x)))))^2)
+rhsfit <- function(varray, mydata) 
+{
+  y.pred = varray[1] + (varray[2] * mydata$x) + (varray[3] *(exp(-varray[4]*mydata$x)));
+  RSS = sum((mydata$y -  y.pred)^2);
 }
 
-# Effect of Soil wate
+# Effect of Soil water stress
 # Richter et al. Agricultural and forest, 109 (2001)
-soilWaterStress = function ()
+# DAW = Daily available water content Qi et al
+# AWC = Available water content
+soilWaterStress = function (DAW, AWC)
 {
   Oth = 0.02; #(Sinclair (1986))
   fdt = 6:12; #(Richter)
@@ -26,6 +30,26 @@ soilWaterStress = function ()
   return(fstress);
   
 }
+
+# Rooting depth (D)
+# Dsowing = Sowing depth fixed
+# T = daily air temperature above 3C
+# l0 = sum of the lenght of epycol
+# B0 = initial relative growth
+# S Rate of change with B0 to 0 = 0.002715
+# T0 = 120
+rootingDepth = function()
+{
+  Dsowing = 0.02;
+  l0 = 0.0491;
+  B0 = 0.00935;
+  S = 0.002715;
+  T0 = 120;
+
+  D = Dsowing + l0*exp((B0/S)*(1-exp(-S*(T-T0))));
+  
+}
+
 
 # Quantitative effect of fstress on y (f on A. Qi)
 effectofSWSfitYield = function(fstress)
@@ -90,8 +114,16 @@ extractParam = function(model)
 fitYield = function(ydat, tdat)
 {
   wdata = data.frame(y=ydat, x=tdat);
-  start1 = c(A = 1, B = 1, C = 0, v=0);
-  eunsc = ydat ~ A + B*tdat + C*(exp(-v*tdat));
+  eunsc = ydat ~ A + B*tdat + C*(exp(-v*tdat)); # Equation
+  
+  # Transform to linear
+  # log(y -A)  = log(Bt) + log(C) - vt;
+  
+  A.0 = min(wdata$y) * 0.5;
+  model.0 = lm(log(y - A.0) ~ x, data=wdata);
+  start1 = c(A = A.0, B = 1, C = exp(coef(model.0)[[1]]), v=coef(model.0)[[2]]); 
+  
+  # v is K in Werker
   optx = optima(start1, wdata, rhsfit);
   o = nlsLM(eunsc, 
             start=list(A = start1[1], B = start1[2], C = start1[3], v = start1[4]), trace = TRUE);
@@ -100,7 +132,6 @@ fitYield = function(ydat, tdat)
   plot(tdat, ydat, xlab = 'T accummulated temperature', 
        ylab = 'foliage cover', main='Model I', ylim=range(-0.4,1), xlim=range(0,2500), pch=21, bg='red');
   lines(tdat, predict(o), col='green')
-  cor(ydat, predict(o))
   
   x = 0:2000;
   yeq = ydat ~ A + B*x + C*(exp(-v*x));
@@ -110,13 +141,6 @@ fitYield = function(ydat, tdat)
 }
 
 
-y = c(0.745882353, 0.731678201, 0.689653979, 0.565986159, 0.553529412, 0.444429066, 0.380415225, 0.261262976
-);
-t = c(1478.125, 912.5,821.875,734.375,734.375,528.125,465.625,443.75);
-fm= fitYield(y, t);
-
-
-
-
-#y0=0.000015; t0=100; u0=0.0050; umax=0.0051; ymax=0.90; kt=1.0; umin=-0.0050; k=0.00346 
-#t = 0:2000;
+ydat = c(0.75, 0.73, 0.69, 0.57, 0.55, 0.44, 0.38, 0.26);
+tdat = c(1478.125, 912.5,821.875,734.375,734.375,528.125,465.625,443.75);
+fm= fitYield(ydat, tdat);
